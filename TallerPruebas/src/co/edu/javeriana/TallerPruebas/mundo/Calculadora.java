12e992661bfed7ca88c481d3092f000c97471c89
Alejandro Guayara <ca.guayara93@gmail.com>
2018-05-07 22:43:38 -0500

    Taller de pruebas

---
+++ TallerPruebas/src/co/edu/javeriana/TallerPruebas/mundo/Calculadora.java
@@ -0,0 +1,31 @@
+package co.edu.javeriana.TallerPruebas.mundo;
+
+public class Calculadora {
+	
+	/**
+	
+	
+		TODO - D - YA TERMINASTE LA CLASE DE PRUEBA? :3
+	
+	
+	*/
+	
+	public Calculadora( ) {
+		
+	}
+	
+	// TODO - E - Metodo Suma
+	
+	// TODO - F - Metodo Resta
+	
+	// TODO - G - Metodo Multiplicacion
+	
+	// TODO - H - Metodo Division
+	
+	// TODO - I -Metodo Palindrome
+
+	public static void main(String[] args) {
+		// TODO - J - Puesta en marcha :D
+	}
+
+}
---
+++ TallerPruebas/testSrc/co/edu/javeriana/TallerPruebas/pruebas/CalculadoraTest.java
@@ -0,0 +1,9 @@
+package co.edu.javeriana.TallerPruebas.pruebas;
+
+public class CalculadoraTest {
+	// TODO - A - Definicion de atributos
+	
+	// TODO - B - Inicializacion de atributos
+	
+	// TODO - C - Definicion de pruebas
+}
